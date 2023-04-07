e9afd1d9b64ea26da5e927168914abdf38a0d889
nvtai <nvtai0202@gmail.com>
2023-04-07 23:06:32 +0700

    dht11

---
+++ dht11_firebase/dht11_firebase.ino
@@ -0,0 +1,58 @@
+#include <FirebaseESP8266.h>
+#include <DHT.h>
+#include  <ESP8266WiFi.h>
+
+#define FIREBASE_HOST "https://iot-dt3-default-rtdb.firebaseio.com"
+#define FIREBASE_AUTH "TpDdtrH4cX2UADvMvaqwoCEwbwEblix9yO7OYOxz"
+#define WIFI_SSID "huy" 
+#define WIFI_PASSWORD "0979311380" 
+#define DHTPIN 2  
+#define DHTTYPE DHT11  
+
+DHT dht(DHTPIN, DHTTYPE);
+FirebaseData fbdo;
+
+void setup() {
+
+  Serial.begin(9600);
+  delay(1000);
+  WiFi.begin (WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Dang ket noi");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  dht.begin();
+  Serial.println ("");
+  Serial.println ("Da ket noi WiFi!");
+  Serial.println(WiFi.localIP());
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+
+}
+
+void loop() {
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();  
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+
+  }
+
+  Serial.print("Nhiet do: ");
+  Serial.print(t);
+  Serial.print("*C  ");
+  Serial.print("Do am: ");
+  Serial.print(h);
+  Serial.println("%  ");
+
+  Firebase.setFloat( fbdo,"Nhiet do", t);
+
+  Firebase.setFloat ( fbdo,"Do am", h);
+
+  delay(200);
+
+}
